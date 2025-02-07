a50fe01080e22f43a1f94de3b28ab3c38ef1e54c
조근완 <kwjo@mqnic.com>
2025-02-07 10:08:01 +0900

    DOC: README.md에 프로젝트 기능 정의

---
+++ todo-map/src/main/java/com/hoan/todomap/TodoMapApplication.java
@@ -0,0 +1,13 @@
+package com.hoan.todomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMapApplication.class, args);
+    }
+
+}
---
+++ todo-map/src/test/java/com/hoan/todomap/TodoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoan.todomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
