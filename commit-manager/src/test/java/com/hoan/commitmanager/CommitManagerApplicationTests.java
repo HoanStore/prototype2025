3a6fe94a7342296973bbe2fe2d1d37f397825c0c
조근완 <kwjo@mqnic.com>
2025-03-20 10:11:26 +0900

    🎉init: commit manager 시작

---
+++ commit-manager/src/test/java/com/hoan/commitmanager/CommitManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoan.commitmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
