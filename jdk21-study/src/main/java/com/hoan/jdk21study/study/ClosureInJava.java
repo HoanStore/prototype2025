0d11ef356da014f239c3aaf5a9355d661f08a887
조근완 <HoanStore701@gmail.com>
2025-05-11 13:32:25 +0900

    study: 자바에서 클로저 예제 작성

---
+++ jdk21-study/src/main/java/com/hoan/jdk21study/study/ClosureInJava.java
@@ -0,0 +1,14 @@
+package com.hoan.jdk21study.study;
+
+import java.util.function.Consumer;
+
+public class ClosureInJava {
+    public static Consumer<String> greeting(String prefix) {
+        return (hello) -> System.out.println(prefix + " " + hello);
+    }
+
+    public static void main(String[] args) {
+        Consumer<String> sayHi = greeting("고객님");
+        sayHi.accept("안녕");
+    }
+}
