011dc39e50131c3fb0b5298bc7df3e0795819d16
조근완 <HoanStore701@gmail.com>
2025-07-18 15:24:39 +0900

    study: jdk18, jwebserver 학습, Node.js 없이도 빠르게 웹서버 띄울 수 있음

---
+++ jdk21-study/src/main/java/com/hoan/jdk21study/study/jdk18/JwebServer.java
@@ -0,0 +1,10 @@
+package com.hoan.jdk21study.study.jdk18;
+
+
+/**
+ * 아래 명령어로 간단하게 web 서버를 기동시킬 수 있다.
+ *
+ * jwebserver -d ~/Documents/prototype-upload2025 --port 8000
+ */
+public class JwebServer {
+}
